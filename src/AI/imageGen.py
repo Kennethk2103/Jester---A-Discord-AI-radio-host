c3c809497de7f47fb00a7493778500fb8cb4a1c4
Kennethk2103 <Kennethk2103@gmail.com>
2025-07-16 17:52:41 -0400

    fixed issues

--- src/AI/imageGen.py
+++ src/AI/imageGen.py
@@ -2,8 +2,13 @@ from diffusers import DiffusionPipeline
 import torch
 import sys
 import os
+
+
+
 pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 pipe.enable_model_cpu_offload()
+
+
 
 # if using torch < 2.0
 # pipe.enable_xformers_memory_efficient_attention()
@@ -12,7 +17,6 @@ pipe.enable_model_cpu_offload()
 prompt = sys.argv[1]
 print(f"Prompt: {prompt}")
 
-
-images = pipe(prompt=prompt).images[0]
+images = pipe(prompt).images[0]
 
 images.save("./output.png")
